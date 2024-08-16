f5ee6caa368a3e69a103a677bc859c6630b5c6d2
rajeshjaga <rajaga223@gmail.com>
2024-08-16 15:57:47 +0530

    commit: new suckless tools added

---
+++ blocks.def.h
@@ -0,0 +1,18 @@
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
+static const Block blocks[] = {
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"", "curl 'https://wttr.in/bangalore?format=3' | xargs echo", 3600, 0},
+    {"Vol: ",
+     "pactl get-sink-volume @DEFAULT_SINK@ | grep -E -o '[0-9]*%' | xargs echo "
+     "| awk '$1 == $2 {print $1} $1 != $2 {print \"check vol\"}'",
+     0, 10},
+    {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {"Bat: ", "acpi -b | cut -d \",\" -f 2 | xargs echo", 5, 0},
+    {"", "date '+%b %d (%a) %I:%M%p'", 5, 0},
+};
+
+// sets delimiter between status commands. NULL character ('\0') means no
+// delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
